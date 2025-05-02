3e62c80551cf25c8e13ba3b435108dfcc8082930
Leon Salsiccia <leon.salsiccia@gmail.com>
2025-05-02 14:25:36 +1000

    refactor: rewrite ResultLike type

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,29 @@ export { flow, pipe } from "./utils";
 type DistributeOk<A> = A extends Ok<infer U> ? (U extends unknown ? Ok<U> : never) : never;
 type DistributeErr<E> = E extends Err<infer U> ? (U extends unknown ? Err<U> : never) : never;
 
-type ResultLike<A = never, E = never> = DistributeOk<Ok<A>> | DistributeErr<Err<E>>;
-type ResultLikeIso<A = never, E = never> = ResultLike<A, E> | Promise<ResultLike<A, E>>;
+type ResultLikeAwaited<A = never, E = never> = DistributeOk<Ok<A>> | DistributeErr<Err<E>>;
+type ResultLikePromise<A = never, E = never> = Promise<ResultLikeAwaited<A, E>>;
+
+export type ResultLike<A, E> = Result<A, E> | ResultLikePromise<A, E> | ResultLikeAwaited<A, E>;
+
+type InferResult<R extends ResultLike<any, any>> =
+  R extends Result<infer A, infer E>
+    ? [A, E]
+    : R extends Promise<infer P>
+      ? P extends Ok<infer A>
+        ? [A, never]
+        : P extends Err<infer E>
+          ? [never, E]
+          : P extends Ok<infer A> | Err<infer E>
+            ? [A, E]
+            : never
+      : R extends Ok<infer A>
+        ? [A, never]
+        : R extends Err<infer E>
+          ? [never, E]
+          : R extends Ok<infer A> | Err<infer E>
+            ? [A, E]
+            : never;
 
 export interface Ok<A> {
   readonly isOk: true;
@@ -57,10 +78,10 @@ const die = (value: unknown): Result<never, never> => {
 
 export namespace Result {
   /* Extracts the Ok channel of a Result */
-  export type InferOk<Y> = Y extends Result<infer A, any> ? A : Y extends Ok<infer A> ? A : never;
+  export type InferOk<R extends ResultLike<any, any>> = InferResult<R>[0];
 
   /* Extracts the Err channel of a Result */
-  export type InferErr<Y> = Y extends Result<any, infer E> ? E : Y extends Err<infer E> ? E : never;
+  export type InferErr<R extends ResultLike<any, any>> = InferResult<R>[1];
 
   /* Extracts the tags of an error type */
   export type TagsOf<E> =
@@ -138,7 +159,7 @@ export class Result<A = never, E = never> implements PromiseLike<Ok<A> | Err<E>>
   /* -------------------------------------------------- */
 
   // ---- unwrap -----------------------------------------------------------------------------
-  static unwrap = <A>(self: Result<A, never> | ResultLikeIso<A, never>): Promise<A> =>
+  static unwrap = <A>(self: ResultLike<A, never>): Promise<A> =>
     Promise.resolve(self).then((r) => {
       if (r.isError) {
         throw new Error("Cannot unwrap an Err value");
@@ -150,14 +171,14 @@ export class Result<A = never, E = never> implements PromiseLike<Ok<A> | Err<E>>
 
   // ---- unwrapOr --------------------------------------------------------------------------
   static unwrapOr = dual<
-    <A2>(fallback: A2) => <A = never, E = never>(self: Result<A, E> | ResultLikeIso<A, E>) => Promise<A | A2>,
-    <A2, A = never, E = never>(self: Result<A, E> | ResultLikeIso<A, E>, fallback: A2) => Promise<A | A2>
+    <A2>(fallback: A2) => <A = never, E = never>(self: ResultLike<A, E>) => Promise<A | A2>,
+    <A2, A = never, E = never>(self: ResultLike<A, E>, fallback: A2) => Promise<A | A2>
   >(2, (self, fallback) => Promise.resolve(self).then((r) => (r.isOk ? r.value : fallback)));
 
   unwrapOr = Result.unwrapOr.bind(null, this) as <A2>(fallback: A2) => Promise<A | A2>;
 
   // ---- unwrapAsTuple ---------------------------------------------------------------------
-  static unwrapAsTuple = <R extends Result<any, any> | ResultLikeIso<any, any>>(
+  static unwrapAsTuple = <R extends ResultLike<any, any>>(
     self: R,
   ): Promise<[Result.InferErr<R>, null] | [null, Result.InferOk<R>]> =>
     Promise.resolve(self).then((r) => (r.isOk ? [null, r.value] : [r.error, null]));
@@ -168,16 +189,16 @@ export class Result<A = never, E = never> implements PromiseLike<Ok<A> | Err<E>>
 
   // ---- map -------------------------------------------------------------------------------
   static map = dual<
-    <A2, A = never>(cb: (value: A) => A2) => <E = never>(self: Result<A, E> | ResultLikeIso<A, E>) => Result<A2, E>,
-    <A2, A = never, E = never>(self: Result<A, E> | ResultLikeIso<A, E>, cb: (value: A) => A2) => Result<A2, E>
+    <A2, A = never>(cb: (value: A) => A2) => <E = never>(self: ResultLike<A, E>) => Result<A2, E>,
+    <A2, A = never, E = never>(self: ResultLike<A, E>, cb: (value: A) => A2) => Result<A2, E>
   >(2, (self, cb) => new Result(Promise.resolve(self).then((r) => (r.isOk ? _ok(cb(r.value)) : r))));
 
   map = Result.map.bind(null, this) as <A2>(cb: (value: A) => A2) => Result<A2, E>;
 
   // ---- mapError --------------------------------------------------------------------------
   static mapError = dual<
-    <E2, E = never>(cb: (e: E) => E2) => <A = never>(self: Result<A, E> | ResultLikeIso<A, E>) => Result<A, E2>,
-    <E2, A = never, E = never>(self: Result<A, E> | ResultLikeIso<A, E>, cb: (e: E) => E2) => Result<A, E2>
+    <E2, E = never>(cb: (e: E) => E2) => <A = never>(self: ResultLike<A, E>) => Result<A, E2>,
+    <E2, A = never, E = never>(self: ResultLike<A, E>, cb: (e: E) => E2) => Result<A, E2>
   >(2, (self, cb) => new Result(Promise.resolve(self).then((r) => (r.isError ? _err(cb(r.error)) : r))));
 
   mapError = Result.mapError.bind(null, this) as <E2>(cb: (e: E) => E2) => Result<A, E2>;
@@ -186,9 +207,9 @@ export class Result<A = never, E = never> implements PromiseLike<Ok<A> | Err<E>>
   static flatMap = dual<
     <A = never, A2 = never, E2 = never>(
       cb: (a: A) => Result<A2, E2> | A2,
-    ) => <E = never>(self: Result<A, E> | ResultLikeIso<A, E>) => Result<A2, E | E2>,
+    ) => <E = never>(self: ResultLike<A, E>) => Result<A2, E | E2>,
     <A = never, E = never, A2 = never, E2 = never>(
-      self: Result<A, E> | ResultLikeIso<A, E>,
+      self: ResultLike<A, E>,
       cb: (a: A) => Result<A2, E2> | A2,
     ) => Result<A2, E | E2>
   >(
@@ -217,9 +238,9 @@ export class Result<A = never, E = never> implements PromiseLike<Ok<A> | Err<E>>
   static tap = dual<
     <A = never, E2 = never>(
       cb: (a: A) => Result<never, E2> | void,
-    ) => <E = never>(self: Result<A, E> | ResultLikeIso<A, E>) => Result<A, E | E2>,
+    ) => <E = never>(self: ResultLike<A, E>) => Result<A, E | E2>,
     <A = never, E = never, E2 = never>(
-      self: Result<A, E> | ResultLikeIso<A, E>,
+      self: ResultLike<A, E>,
       cb: (a: A) => Result<never, E2> | void,
     ) => Result<A, E | E2>
   >(
@@ -238,11 +259,11 @@ export class Result<A = never, E = never> implements PromiseLike<Ok<A> | Err<E>>
 
   // ---- tapError --------------------------------------------------------------------------
   static tapError = dual<
-    <E2, E = never>(
+    <E = never, E2 = never>(
       cb: (e: E) => Result<unknown, E2> | void,
-    ) => <A = never>(self: Result<A, E> | ResultLikeIso<A, E>) => Result<A, E | E2>,
+    ) => <A = never>(self: ResultLike<A, E>) => Result<A, E | E2>,
     <A = never, E = never, E2 = never>(
-      self: Result<A, E> | ResultLikeIso<A, E>,
+      self: ResultLike<A, E>,
       cb: (e: E) => Result<unknown, E2> | void,
     ) => Result<A, E | E2>
   >(
@@ -266,9 +287,9 @@ export class Result<A = never, E = never> implements PromiseLike<Ok<A> | Err<E>>
     <const Tag extends Result.TagsOf<E>, E = never, E2 = never>(
       tag: Tag,
       cb: (err: Extract<E, { _tag: Tag }>) => Result<unknown, E2> | void,
-    ) => <A = never>(self: Result<A, E> | ResultLikeIso<A, E>) => Result<A, E | E2>,
+    ) => <A = never>(self: ResultLike<A, E>) => Result<A, E | E2>,
     <const Tag extends Result.TagsOf<E>, A = never, E = never, E2 = never>(
-      self: Result<A, E> | ResultLikeIso<A, E>,
+      self: ResultLike<A, E>,
       tag: Tag,
       cb: (err: Extract<E, { _tag: Tag }>) => Result<unknown, E2> | void,
     ) => Result<A, E | E2>
@@ -293,13 +314,8 @@ export class Result<A = never, E = never> implements PromiseLike<Ok<A> | Err<E>>
 
   // ---- orElse ----------------------------------------------------------------------------
   static orElse = dual<
-    <A2, E2, E = never>(
-      cb: (e: E) => Result<A2, E2>,
-    ) => <A = never>(self: Result<A, E> | ResultLikeIso<A, E>) => Result<A | A2, E2>,
-    <A2, E2, A = never, E = never>(
-      self: Result<A, E> | ResultLikeIso<A, E>,
-      cb: (e: E) => Result<A2, E2>,
-    ) => Result<A | A2, E2>
+    <A2, E2, E = never>(cb: (e: E) => Result<A2, E2>) => <A = never>(self: ResultLike<A, E>) => Result<A | A2, E2>,
+    <A2, E2, A = never, E = never>(self: ResultLike<A, E>, cb: (e: E) => Result<A2, E2>) => Result<A | A2, E2>
   >(2, (self, cb) => new Result(Promise.resolve(self).then(async (r) => (r.isOk ? r : await cb(r.error)))));
 
   orElse = Result.orElse.bind(null, this) as <A2, E2>(cb: (e: E) => Result<A2, E2>) => Result<A | A2, E2>;
@@ -309,9 +325,9 @@ export class Result<A = never, E = never> implements PromiseLike<Ok<A> | Err<E>>
     <E, const Tag extends Result.TagsOf<E>, A2, E2>(
       tag: Tag,
       cb: (e: Extract<E, { _tag: Tag }>) => Result<A2, E2>,
-    ) => <A = never>(self: Result<A, E> | Err<E>) => Result<A | A2, Exclude<E, { _tag: Tag }> | E2>,
+    ) => <A = never>(self: ResultLike<A, E> | Err<E>) => Result<A | A2, Exclude<E, { _tag: Tag }> | E2>,
     <E, const Tag extends Result.TagsOf<E>, A2, E2, A = never>(
-      self: Result<A, E> | Err<E>,
+      self: ResultLike<A, E> | Err<E>,
       tag: Tag,
       cb: (e: Extract<E, { _tag: Tag }>) => Result<A2, E2>,
     ) => Result<A | A2, Exclude<E, { _tag: Tag }> | E2>
@@ -347,7 +363,7 @@ export class Result<A = never, E = never> implements PromiseLike<Ok<A> | Err<E>>
       E = never,
     >(
       cases: Cases,
-    ) => (self: Result<A, E> | ResultLikeIso<A, E>) => Result<
+    ) => (self: ResultLike<A, E>) => Result<
       | A
       | {
           [K in keyof Cases]: Cases[K] extends (...args: Array<any>) => Result<infer A, any> ? A : never;
@@ -368,7 +384,7 @@ export class Result<A = never, E = never> implements PromiseLike<Ok<A> | Err<E>>
       A = never,
       E = never,
     >(
-      self: Result<A, E> | ResultLikeIso<A, E>,
+      self: ResultLike<A, E>,
       cases: Cases,
     ) => Result<
       | A
@@ -523,29 +539,30 @@ export function isError(value: any, tag?: string): boolean {
 /*  Utility                                           */
 /* -------------------------------------------------- */
 
-function try_<A, E>(options: { try: () => A; catch: (e: unknown) => E }): Result<Awaited<A>, E>;
-function try_<A>(cb: () => A): Result<Awaited<A>, UnknownException>;
-function try_<A, E>(arg: (() => A) | { try: () => A; catch: (e: unknown) => E }) {
-  const tryable = typeof arg === "function" ? arg : arg.try;
-  const catchable = typeof arg === "function" ? (e: unknown) => new UnknownException({ cause: e }) : arg.catch;
+function try_<A, E>(tryFn: () => A, catchFn: (e: unknown) => E): Result<Awaited<A>, E>;
+function try_<A>(tryFn: () => A): Result<Awaited<A>, UnknownException>;
+function try_<A, E>(tryFn: () => A, catchFn?: (e: unknown) => E) {
+  const handleError = catchFn || ((e: unknown) => new UnknownException({ cause: e }));
 
   try {
     return new Result(
-      Promise.resolve(tryable()).then(
+      Promise.resolve(tryFn()).then(
         (r) => _ok(r),
-        (e) => _err(catchable(e)),
+        (e) => _err(handleError(e)),
       ),
     );
   } catch (e) {
-    return new Result(Promise.resolve(_err(catchable(e))));
+    return new Result(Promise.resolve(_err(handleError(e))));
   }
 }
 
-function all<const R extends readonly Result<any, any>[]>(
+export type ResultLikeOmni<A, E> = Promise<Ok<A>> | Promise<Err<E>>;
+
+function all<const R extends readonly ResultLike<any, any>[]>(
   results: R,
 ): Result<
-  { [K in keyof R]: R[K] extends Result<infer A, any> ? A : never },
-  { [K in number]: R[K] extends Result<any, infer E> ? E : never }[number]
+  { [K in keyof R]: R[K] extends ResultLike<any, any> ? Result.InferOk<R[K]> : never },
+  { [K in number]: R[K] extends ResultLike<any, any> ? Result.InferErr<R[K]> : never }[number]
 > {
   return new Result(
     Promise.all(results).then((results) => {
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,13 +1,13 @@
 /**
  * Comprehensive test‑suite for `@lonjonn/fallible`
  * ------------------------------------------------
- * The scenarios mimic a tiny “banking” domain to exercise every public
+ * The scenarios mimic a tiny "banking" domain to exercise every public
  * surface‑area of the library with semi‑realistic flows rather than
  * contrived `TestError` values.
  *
  * Runtime assertions use Vitest; type‑level assertions use `expectTypeOf`.
  *
- * What’s covered?
+ * What's covered?
  *  • Static helpers — ok, err, die, gen, TaggedError, isError, try, all
  *  • Instance helpers — unwrap(+Overloads), map/flatMap/tap chain, etc.
  *  • Type utilities   — InferOk/InferErr/TagsOf, conditional unwrap, …
@@ -84,6 +84,27 @@ describe("🔡  Type utilities", () => {
     type Mixed = Ok<string> | Err<NetworkError> | Err<DatabaseError>;
     expectTypeOf<Result.InferOk<Mixed>>().toEqualTypeOf<string>();
     expectTypeOf<Result.InferErr<Mixed>>().toEqualTypeOf<NetworkError | DatabaseError>();
+
+    expectTypeOf<Result.InferErr<Result<number, string>>>().toEqualTypeOf<string>();
+    expectTypeOf<Result.InferOk<Result<number, string>>>().toEqualTypeOf<number>();
+
+    expectTypeOf<Result.InferErr<Promise<Ok<number>>>>().toEqualTypeOf<never>();
+    expectTypeOf<Result.InferOk<Promise<Ok<number>>>>().toEqualTypeOf<number>();
+
+    expectTypeOf<Result.InferErr<Promise<Err<string>>>>().toEqualTypeOf<string>();
+    expectTypeOf<Result.InferOk<Promise<Err<string>>>>().toEqualTypeOf<never>();
+
+    expectTypeOf<Result.InferErr<Promise<Ok<number> | Err<string>>>>().toEqualTypeOf<string>();
+    expectTypeOf<Result.InferOk<Promise<Ok<number> | Err<string>>>>().toEqualTypeOf<number>();
+
+    expectTypeOf<Result.InferErr<Ok<number>>>().toEqualTypeOf<never>();
+    expectTypeOf<Result.InferOk<Ok<number>>>().toEqualTypeOf<number>();
+
+    expectTypeOf<Result.InferErr<Err<string>>>().toEqualTypeOf<string>();
+    expectTypeOf<Result.InferOk<Err<string>>>().toEqualTypeOf<never>();
+
+    expectTypeOf<Result.InferErr<Ok<number> | Err<string>>>().toEqualTypeOf<string>();
+    expectTypeOf<Result.InferOk<Ok<number> | Err<string>>>().toEqualTypeOf<number>();
   });
 
   it("TagsOf collects `_tag` literals deeply", () => {
@@ -103,11 +124,13 @@ describe("🔡  Type utilities", () => {
 
 describe("🏗️  Construction helpers", () => {
   it("ok / err produce the right tagged objects", async () => {
-    const o = await ok(42);
-    const e = await err("nope");
+    const o = ok(42);
+    expectTypeOf(o).toEqualTypeOf<Result<number, never>>();
+    expect(await o).toMatchObject({ isOk: true, value: 42 });
 
-    expect(o).toMatchObject({ isOk: true, value: 42 });
-    expect(e).toMatchObject({ isError: true, error: "nope" });
+    const e = err("nope");
+    expectTypeOf(e).toEqualTypeOf<Result<never, string>>();
+    expect(await e).toMatchObject({ isError: true, error: "nope" });
   });
 
   it("die throws immediately", () => {
@@ -117,12 +140,14 @@ describe("🏗️  Construction helpers", () => {
   it("async iterator semantics", async () => {
     /* ok values yield nothing & return value */
     const o = ok("yes");
+    expectTypeOf(o).toEqualTypeOf<Result<string, never>>();
     const vals: any[] = [];
     for await (const v of o) vals.push(v);
     expect(vals).toEqual([]);
 
     /* err values yield themselves */
     const e = err("fail");
+    expectTypeOf(e).toEqualTypeOf<Result<never, string>>();
     let yielded;
     for await (const v of e) yielded = v;
     expect(yielded).toEqual(expect.objectContaining({ error: "fail" }));
@@ -135,33 +160,35 @@ describe("🏗️  Construction helpers", () => {
 
 describe("🔀  Combinators", () => {
   it("map transforms Ok & preserves Err", async () => {
-    expect(
-      await pipe(
-        ok(2),
-        Result.map((n) => n * 2),
-      ),
-    ).toMatchObject({ isOk: true, value: 4 });
-    expect(
-      await pipe(
-        err("x"),
-        Result.map((n: number) => n * 2),
-      ),
-    ).toMatchObject({ isError: true, error: "x" });
+    const mapped = pipe(
+      ok(2),
+      Result.map((n) => n * 2),
+    );
+    expectTypeOf(mapped).toEqualTypeOf<Result<number, never>>();
+    expect(await mapped).toMatchObject({ isOk: true, value: 4 });
+
+    const preserved = pipe(
+      err("x"),
+      Result.map((n) => n * 2),
+    );
+    expectTypeOf(preserved).toEqualTypeOf<Result<number, string>>();
+    expect(await preserved).toMatchObject({ isError: true, error: "x" });
   });
 
   it("mapError transforms Err & preserves Ok", async () => {
-    const upper = await pipe(
+    const upper = pipe(
       err("bad"),
       Result.mapError((s) => s.toUpperCase()),
     );
-    expect(upper).toMatchObject({ error: "BAD" });
+    expectTypeOf(upper).toEqualTypeOf<Result<never, string>>();
+    expect(await upper).toMatchObject({ error: "BAD" });
 
-    expect(
-      await pipe(
-        ok(1),
-        Result.mapError(() => new Error()),
-      ),
-    ).toMatchObject({ value: 1 });
+    const preserved = pipe(
+      ok(1),
+      Result.mapError(() => new Error()),
+    );
+    expectTypeOf(preserved).toEqualTypeOf<Result<number, Error>>();
+    expect(await preserved).toMatchObject({ value: 1 });
   });
 
   it("flatMap chains & merges error unions", async () => {
@@ -174,29 +201,37 @@ describe("🔀  Combinators", () => {
   });
 
   it("tap / tapError allow side‑effects", async () => {
-    const inSpy = vi.fn();
-    const errSpy = vi.fn();
+    const inSpy = vi.fn(() => {});
+    const errSpy = vi.fn(() => {});
 
-    const out1 = await pipe(ok("🎉"), Result.tap(inSpy));
+    const out1 = pipe(ok("🎉"), Result.tap(inSpy));
+    expect((await out1).value).toBe("🎉");
     expect(inSpy).toHaveBeenCalledWith("🎉");
-    expect(out1.value).toBe("🎉");
+    expectTypeOf(out1).toEqualTypeOf<Result<string, never>>();
 
-    const out2 = await pipe(err("💥"), Result.tapError(errSpy));
+    const out2 = pipe(err("💥"), Result.tapError(errSpy));
+    expect((await out2).error).toBe("💥");
     expect(errSpy).toHaveBeenCalledWith("💥");
-    expect(out2.error).toBe("💥");
+    expectTypeOf(out2).toEqualTypeOf<Result<never, string>>();
   });
 
   it("orElse provides fallback", async () => {
-    const r = await pipe(
+    const r = pipe(
       err(new NetworkError({ status: 500, body: "oops" })),
       Result.orElse((e) => ok(`Recovered from ${e._tag}`)),
     );
-    expect(r).toMatchObject({ isOk: true, value: "Recovered from NetworkError" });
+    expectTypeOf(r).toEqualTypeOf<Result<string, never>>();
+    expect(await r).toMatchObject({ isOk: true, value: "Recovered from NetworkError" });
   });
 
   it("tapErrorTag fires only on matching tag", async () => {
     const spy = vi.fn(() => {});
-    await pipe(err(new ValidationError({ field: "x", issue: "bad" })), Result.tapErrorTag("ValidationError", spy));
+    const result = pipe(
+      err(new ValidationError({ field: "x", issue: "bad" })),
+      Result.tapErrorTag("ValidationError", spy),
+    );
+    expectTypeOf(result).toEqualTypeOf<Result<never, ValidationError>>();
+    await result;
     expect(spy).toHaveBeenCalledTimes(1);
 
     spy.mockClear();
@@ -206,15 +241,16 @@ describe("🔀  Combinators", () => {
   });
 
   it("catchTag converts specific errors", async () => {
-    const r = await pipe(
+    const r = pipe(
       err(new ValidationError({ field: "age", issue: "negative" })),
       Result.catchTag("ValidationError", (v) => ok(`${v.field}: ${v.issue}`)),
     );
-    expect(r).toMatchObject({ value: "age: negative" });
+    expectTypeOf(r).toEqualTypeOf<Result<string, never>>();
+    expect(await r).toMatchObject({ value: "age: negative" });
   });
 
   it("catchTags handles multiple error codes", async () => {
-    const r = await pipe(
+    const r = pipe(
       err(
         Math.random()
           ? new NetworkError({ status: 404, body: "not found" })
@@ -225,19 +261,31 @@ describe("🔀  Combinators", () => {
         ValidationError: (v) => ok(v.issue),
       }),
     );
-    expect(r).toMatchObject({ value: "HTTP 404" });
+    expectTypeOf(r).toEqualTypeOf<Result<string, never>>();
+    expect(await r).toMatchObject({ value: "HTTP 404" });
   });
 
   it("unwrap / unwrapOr / unwrapAsTuple", async () => {
-    const u = await ok(9).unwrap();
+    const okResult = ok(9);
+    const unwrapped = okResult.unwrap();
+    expectTypeOf(unwrapped).toEqualTypeOf<Promise<number>>();
+    const u = await unwrapped;
     expect(u).toBe(9);
 
-    const v1 = await ok(7).unwrapOr("fallback");
-    const v2 = await err("x").unwrapOr("fallback");
+    const v1Promise = ok(7).unwrapOr("fallback");
+    expectTypeOf(v1Promise).toEqualTypeOf<Promise<number | string>>();
+    const v1 = await v1Promise;
+
+    const v2Promise = err("x").unwrapOr("fallback");
+    expectTypeOf(v2Promise).toEqualTypeOf<Promise<string>>();
+    const v2 = await v2Promise;
+
     expect(v1).toBe(7);
     expect(v2).toBe("fallback");
 
-    const tuple = await ok(3).unwrapAsTuple();
+    const tuplePromise = ok(3).unwrapAsTuple();
+    expectTypeOf(tuplePromise).toEqualTypeOf<Promise<[never, null] | [null, number]>>();
+    const tuple = await tuplePromise;
     expect(tuple).toEqual([null, 3]);
   });
 });
@@ -249,17 +297,23 @@ describe("🔀  Combinators", () => {
 describe("🚦  isError type‑guard", () => {
   it("narrows Err generically", async () => {
     const r = await (Math.random() > 0.5 ? ok("hi") : err(new DatabaseError({ query: "q" })));
+    expectTypeOf(r).toEqualTypeOf<Ok<string> | Err<DatabaseError>>();
+
     if (isError(r)) {
       expectTypeOf(r.error).toEqualTypeOf<DatabaseError>();
       expect(r.error.query).toBe("q");
     } else {
+      expectTypeOf(r.value).toEqualTypeOf<string>();
       expect(r.value).toBe("hi");
     }
   });
 
   it("narrows specific tags", async () => {
     const r = await err(new DatabaseError({ query: "DROP" }));
+    expectTypeOf(r).toEqualTypeOf<Err<DatabaseError>>();
+
     if (isError(r, "DatabaseError")) {
+      expectTypeOf(r.error).toEqualTypeOf<DatabaseError>();
       expect(r.error.query).toBe("DROP");
     }
   });
@@ -278,13 +332,21 @@ describe("🔄  Result.gen", () => {
     return { from: updatedFrom, to: updatedTo };
   });
 
+  expectTypeOf(transfer).toBeFunction();
+
   it("succeeds end‑to‑end when data happy", async () => {
     const r = await transfer(1, 1, 10); // same user to avoid 404 for toId
+    expectTypeOf(r).toEqualTypeOf<
+      Ok<{ from: User; to: User }> | Err<NetworkError> | Err<ValidationError> | Err<DatabaseError>
+    >();
     expect(r.isOk && r.value.from.balance).toBe(90);
   });
 
   it("short‑circuits on first error", async () => {
     const r = await transfer(999, 1, 5); // 999 triggers 404
+    expectTypeOf(r).toEqualTypeOf<
+      Ok<{ from: User; to: User }> | Err<NetworkError> | Err<ValidationError> | Err<DatabaseError>
+    >();
     expect(r.isError && r.error._tag).toBe("NetworkError");
   });
 
@@ -293,7 +355,10 @@ describe("🔄  Result.gen", () => {
       yield* new DatabaseError({ query: "DROP" });
       return "bar";
     });
+    expectTypeOf(serialisable).toBeFunction();
+
     const r = await serialisable();
+    expectTypeOf(r).toEqualTypeOf<Ok<string> | Err<DatabaseError>>();
     expect(r.isError && r.error).not.toBeInstanceOf(DatabaseError);
     expect(r.isError && r.error._tag).toBe("DatabaseError");
   });
@@ -304,34 +369,50 @@ describe("🔄  Result.gen", () => {
 /* -------------------------------------------------- */
 
 describe("🧰  Utility functions", () => {
-  it("try wraps sync code — success", async () => {
+  it(".try() wraps sync code — success", async () => {
     const r = await Result.try(() => 7);
+    expectTypeOf(r).toEqualTypeOf<Ok<number> | Err<UnknownException>>();
     expect(r).toMatchObject({ value: 7 });
   });
 
-  it("try wraps sync code — failure into UnknownException", async () => {
+  it(".try() wraps sync code — failure into UnknownException", async () => {
     const r = await Result.try(() => {
       throw new Error("oops");
     });
+    expectTypeOf(r).toEqualTypeOf<Err<UnknownException>>();
     expect(r.isError && r.error).toBeInstanceOf(UnknownException);
   });
 
-  it("try with custom catcher", async () => {
-    const r = await Result.try({
-      try: () => {
+  it(".try() with custom catcher", async () => {
+    const r = await Result.try(
+      () => {
         throw "raw";
       },
-      catch: (raw) => new ValidationError({ field: "?", issue: String(raw) }),
-    });
+      (raw) => new ValidationError({ field: "?", issue: String(raw) }),
+    );
+    expectTypeOf(r).toEqualTypeOf<Err<ValidationError>>();
     expect(r.isError && r.error).toBeInstanceOf(ValidationError);
   });
 
-  it("all aggregates Ok values & short‑circuits on Err", async () => {
-    const mixed = await Result.all([ok(1), ok(2), ok(3)]);
-    expect(mixed).toMatchObject({ value: [1, 2, 3] });
+  it(".all() aggregates Ok values & short‑circuits on Err", async () => {
+    const mixed = Result.all([ok(1), ok(2), ok(3)]);
+    expect(await mixed).toMatchObject({ value: [1, 2, 3] });
+    expectTypeOf(mixed).toEqualTypeOf<Result<readonly [number, number, number], never>>();
 
-    const bad = await Result.all([ok(1), err("bork"), ok(3)]);
-    expect(bad).toMatchObject({ error: "bork" });
+    const bad = Result.all([ok(1), err("bork"), ok(3)]);
+    expect(await bad).toMatchObject({ error: "bork" });
+    expectTypeOf(bad).toEqualTypeOf<Result<readonly [number, never, number], string>>();
+  });
+
+  it(".all() supports Result-like", async () => {
+    const fallible = async () => await ok(1);
+
+    const mixed = Result.all([fallible(), fallible(), fallible()]);
+    expect(await mixed).toMatchObject({ value: [1, 1, 1] });
+    expectTypeOf(mixed).toEqualTypeOf<Result<readonly [number, number, number], never>>();
+    const bad = Result.all([ok(1), err("bork"), ok(3)]);
+    expect(await bad).toMatchObject({ error: "bork" });
+    expectTypeOf(bad).toEqualTypeOf<Result<readonly [number, never, number], string>>();
   });
 });
 
@@ -342,6 +423,7 @@ describe("🧰  Utility functions", () => {
 describe("📦  asSerializable", () => {
   it('removes Symbol.asyncIterator (to appease solutions such as Next.js "use cache")', async () => {
     const clean = await ok("clean");
+    expectTypeOf(clean).toEqualTypeOf<Ok<string>>();
     expect(clean[Symbol.asyncIterator]).toBeUndefined();
   });
 });
